64fd8d7678ef9c940638a25ba3f947863850a5d0
CringeCode228 <81523245+CringeCode228@users.noreply.github.com>
2022-05-10 15:32:02 +0300

    ready

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ from flask_migrate import Migrate
 application = Flask(__name__, template_folder="templates", static_folder="static")
 application.config.from_object(config)
 db = SQLAlchemy(application)
-db.create_all()
 loginManager = LoginManager()
 loginManager.init_app(application)
 migrate = Migrate(application, db, render_as_batch=True)
